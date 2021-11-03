98b3db3dadb1e81acc70959276f0fe02ec2534fd
jacky <jc6825@stern.nyu.edu>
2021-11-03 19:03:30 -0400

    Complete project

--- src/index.js
+++ src/index.js
@@ -1,22 +1,17 @@
 import Stock from "./scripts/stock"
 
 document.addEventListener("DOMContentLoaded", () => {
-    // const stock = document.getElementById("stock")
-    // new Stock(stock) 
+ 
     const stockForm = document.querySelector(".stock-form");
     
-    //create an event handler that renders graph 
-    const renderPriceChart = (e) => {
-        e.preventDefault(); //stops the page from refreshing after clicking "get information"
+    const renderStock = (e) => {
+        e.preventDefault(); 
         let input = document.querySelector("input[name='search-stock']");
-        let ticker = input.value; //grabs the ticker from the input 
-        //make a new instance of stock: thereby also rendering the price chart 
+        let ticker = input.value; 
+        
         new Stock(ticker);
     }
     
-    //add eventlistener to stock form 
-    stockForm.addEventListener('submit', renderPriceChart); 
-   
-    
+    stockForm.addEventListener('submit', renderStock); 
 })
 
