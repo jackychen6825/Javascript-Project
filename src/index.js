3795b2519d1d6f76085101072bd9ac48e758d926
jacky <jc6825@stern.nyu.edu>
2021-11-03 14:06:45 -0400

    Complete stock selection

--- src/index.js
+++ src/index.js
@@ -1,7 +1,22 @@
-import Example from "./scripts/example"
+import Stock from "./scripts/stock"
+
 document.addEventListener("DOMContentLoaded", () => {
-    console.log("hello world!")
-    const main = document.getElementById("main")
-    new Example(main) 
+    // const stock = document.getElementById("stock")
+    // new Stock(stock) 
+    const stockForm = document.querySelector(".stock-form");
+    
+    //create an event handler that renders graph 
+    const renderPriceChart = (e) => {
+        e.preventDefault(); //stops the page from refreshing after clicking "get information"
+        let input = document.querySelector("input[name='search-stock']");
+        let ticker = input.value; //grabs the ticker from the input 
+        //make a new instance of stock: thereby also rendering the price chart 
+        new Stock(ticker);
+    }
+    
+    //add eventlistener to stock form 
+    stockForm.addEventListener('submit', renderPriceChart); 
+   
+    
 })
 
